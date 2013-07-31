975fce26b526ed2112bddb5ab05ccea4e9fb5f91
Estanislau Trepat <estanis@etrepat.com>
2013-07-31 20:49:43 +0200

    Bump version.

--- src/Baum/BaumServiceProvider.php
+++ src/Baum/BaumServiceProvider.php
@@ -14,7 +14,7 @@ class BaumServiceProvider extends ServiceProvider {
    *
    * @var string
    */
-  const VERSION = '1.0.5';
+  const VERSION = '1.0.6';
 
   /**
    * Indicates if loading of the provider is deferred.
