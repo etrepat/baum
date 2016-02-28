ec4073a60d0852cf011bfdf530ca8b2e42eeaf85
Decebal Dobrica <decebal.dobrica@gmail.com>
2016-02-28 21:52:39 +0000

    qualified key names

--- src/Baum/Extensions/Eloquent/Model.php
+++ src/Baum/Extensions/Eloquent/Model.php
@@ -6,114 +6,127 @@ use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
 use Baum\Extensions\Query\Builder as QueryBuilder;
 
-abstract class Model extends BaseModel {
+abstract class Model extends BaseModel
+{
 
-  /**
-   * Reloads the model from the database.
-   *
-   * @return \Baum\Node
-   *
-   * @throws ModelNotFoundException
-   */
-  public function reload() {
-    if ( $this->exists || ($this->areSoftDeletesEnabled() && $this->trashed()) ) {
-      $fresh = $this->getFreshInstance();
+    /**
+     * Reloads the model from the database.
+     *
+     * @return \Baum\Node
+     *
+     * @throws ModelNotFoundException
+     */
+    public function reload()
+    {
+        if ($this->exists || ($this->areSoftDeletesEnabled() && $this->trashed())) {
+            $fresh = $this->getFreshInstance();
 
-      if ( is_null($fresh) )
-        throw with(new ModelNotFoundException)->setModel(get_called_class());
+            if (is_null($fresh)) {
+                throw with(new ModelNotFoundException)->setModel(get_called_class());
+            }
 
-      $this->setRawAttributes($fresh->getAttributes(), true);
+            $this->setRawAttributes($fresh->getAttributes(), true);
 
-      $this->setRelations($fresh->getRelations());
+            $this->setRelations($fresh->getRelations());
 
-      $this->exists = $fresh->exists;
-    } else {
-      // Revert changes if model is not persisted
-      $this->attributes = $this->original;
+            $this->exists = $fresh->exists;
+        } else {
+            // Revert changes if model is not persisted
+            $this->attributes = $this->original;
+        }
+
+        return $this;
     }
 
-    return $this;
-  }
+    /**
+     * Get the observable event names.
+     *
+     * @return array
+     */
+    public function getObservableEvents()
+    {
+        return array_merge(array('moving', 'moved'), parent::getObservableEvents());
+    }
 
-  /**
-   * Get the observable event names.
-   *
-   * @return array
-   */
-  public function getObservableEvents() {
-    return array_merge(array('moving', 'moved'), parent::getObservableEvents());
-  }
+    /**
+     * Register a moving model event with the dispatcher.
+     *
+     * @param  Closure|string $callback
+     * @return void
+     */
+    public static function moving($callback, $priority = 0)
+    {
+        static::registerModelEvent('moving', $callback, $priority);
+    }
 
-  /**
-   * Register a moving model event with the dispatcher.
-   *
-   * @param  Closure|string  $callback
-   * @return void
-   */
-  public static function moving($callback, $priority = 0) {
-    static::registerModelEvent('moving', $callback, $priority);
-  }
+    /**
+     * Register a moved model event with the dispatcher.
+     *
+     * @param  Closure|string $callback
+     * @return void
+     */
+    public static function moved($callback, $priority = 0)
+    {
+        static::registerModelEvent('moved', $callback, $priority);
+    }
 
-  /**
-   * Register a moved model event with the dispatcher.
-   *
-   * @param  Closure|string  $callback
-   * @return void
-   */
-  public static function moved($callback, $priority = 0) {
-    static::registerModelEvent('moved', $callback, $priority);
-  }
+    /**
+     * Get a new query builder instance for the connection.
+     *
+     * @return \Baum\Extensions\Query\Builder
+     */
+    protected function newBaseQueryBuilder()
+    {
+        $conn = $this->getConnection();
 
-  /**
-   * Get a new query builder instance for the connection.
-   *
-   * @return \Baum\Extensions\Query\Builder
-   */
-  protected function newBaseQueryBuilder() {
-    $conn = $this->getConnection();
+        $grammar = $conn->getQueryGrammar();
 
-    $grammar = $conn->getQueryGrammar();
+        return new QueryBuilder($conn, $grammar, $conn->getPostProcessor());
+    }
 
-    return new QueryBuilder($conn, $grammar, $conn->getPostProcessor());
-  }
+    /**
+     * Returns a fresh instance from the database.
+     *
+     * @return \Baum\Node
+     */
+    protected function getFreshInstance()
+    {
+        if ($this->areSoftDeletesEnabled()) {
+            return static::withTrashed()->find($this->getKey());
+        }
 
-  /**
-   * Returns a fresh instance from the database.
-   *
-   * @return \Baum\Node
-   */
-  protected function getFreshInstance() {
-    if ( $this->areSoftDeletesEnabled() )
-      return static::withTrashed()->find($this->getKey());
+        return static::find($this->getKey());
+    }
 
-    return static::find($this->getKey());
-  }
+    /**
+     * Returns wether soft delete functionality is enabled on the model or not.
+     *
+     * @return boolean
+     */
+    public function areSoftDeletesEnabled()
+    {
+        // To determine if there's a global soft delete scope defined we must
+        // first determine if there are any, to workaround a non-existent key error.
+        $globalScopes = $this->getGlobalScopes();
 
-  /**
-   * Returns wether soft delete functionality is enabled on the model or not.
-   *
-   * @return boolean
-   */
-  public function areSoftDeletesEnabled() {
-    // To determine if there's a global soft delete scope defined we must
-    // first determine if there are any, to workaround a non-existent key error.
-    $globalScopes = $this->getGlobalScopes();
+        if (count($globalScopes) === 0) {
+            return false;
+        }
 
-    if ( count($globalScopes) === 0 ) return false;
+        // Now that we're sure that the calling class has some kind of global scope
+        // we check for the SoftDeletingScope existance
+        return static::hasGlobalScope(new SoftDeletingScope);
+    }
 
-    // Now that we're sure that the calling class has some kind of global scope
-    // we check for the SoftDeletingScope existance
-    return static::hasGlobalScope(new SoftDeletingScope);
-  }
-
-  /**
-   * Static method which returns wether soft delete functionality is enabled
-   * on the model.
-   *
-   * @return boolean
-   */
-  public static function softDeletesEnabled() {
-    return with(new static)->areSoftDeletesEnabled();
-  }
+    /**
+     * Static method which returns wether soft delete functionality is enabled
+     * on the model.
+     *
+     * @return boolean
+     */
+    public static function softDeletesEnabled()
+    {
+        return with(new static)->areSoftDeletesEnabled();
+    }
 
 }
--- src/Baum/Extensions/Query/Builder.php
+++ src/Baum/Extensions/Query/Builder.php
@@ -4,36 +4,42 @@ namespace Baum\Extensions\Query;
 
 use Illuminate\Database\Query\Builder as BaseBuilder;
 
-class Builder extends BaseBuilder {
+class Builder extends BaseBuilder
+{
 
-  /**
-   * Replace the "order by" clause of the current query.
-   *
-   * @param  string  $column
-   * @param  string  $direction
-   * @return \Illuminate\Database\Query\Builder|static
-   */
-  public function reOrderBy($column, $direction = 'asc') {
-    $this->orders = null;
+    /**
+     * Replace the "order by" clause of the current query.
+     *
+     * @param  string $column
+     * @param  string $direction
+     * @return \Baum\Extensions\Query\Builder|static
+     */
+    public function reOrderBy($column, $direction = 'asc')
+    {
+        $this->orders = null;
 
-    if ( !is_null($column) ) return $this->orderBy($column, $direction);
+        if (!is_null($column)) {
+            return $this->orderBy($column, $direction);
+        }
 
-    return $this;
-  }
+        return $this;
+    }
 
-  /**
-   * Execute an aggregate function on the database.
-   *
-   * @param  string  $function
-   * @param  array   $columns
-   * @return mixed
-   */
-  public function aggregate($function, $columns = array('*')) {
-    // Postgres doesn't like ORDER BY when there's no GROUP BY clause
-    if ( !isset($this->groups) )
-      $this->reOrderBy(null);
+    /**
+     * Execute an aggregate function on the database.
+     *
+     * @param  string $function
+     * @param  array $columns
+     * @return mixed
+     */
+    public function aggregate($function, $columns = array('*'))
+    {
+        // Postgres doesn't like ORDER BY when there's no GROUP BY clause
+        if (!isset($this->groups)) {
+            $this->reOrderBy(null);
+        }
 
-    return parent::aggregate($function, $columns);
-  }
+        return parent::aggregate($function, $columns);
+    }
 
 }
